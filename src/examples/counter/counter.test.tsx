28d62df7274e5a01725f2fe868a89f1ab365f592
Aryan Jagadale <aryan.j@solarsquare.in>
2025-01-05 10:29:09 +0530

    added tests for counter app

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,29 @@
+// @vitest-environment jsdom
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  // screen.debug(document.body);
+  expect(currentCount.textContent).toBe('0');
 
-test.todo(
+});
+
+test(
   'it should increment when the "Increment" button is pressed',
-  async () => {},
+  async () => {
+    const user = userEvent.setup();
+    render(<Counter />);
+    const incrementButton = screen.getByRole('button', { name: 'Increment' });
+    const currentCount = screen.getByTestId('current-count');
+    await user.click(incrementButton);
+
+    console.log(currentCount);
+    
+    expect(currentCount.textContent).toBe('1');
+
+
+  },
 );
